6e45485be1ff8c78c9ab7a62e30d59744aa775d3
Johannes Wünsche <johannes@spacesnek.rocks>
2021-04-22 11:22:22 +0200

    Implement first two subtasks

--- task2/checkpoint/checkpoint.c
+++ task2/checkpoint/checkpoint.c
@@ -1,6 +1,7 @@
 /* ************************************************************************ */
 /* Include standard header file.                                            */
 /* ************************************************************************ */
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -9,12 +10,48 @@
 #include <sys/time.h>
 #include <time.h>
 #include <omp.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
 
+/* CONSTANTS */
 #define N 360
+#define COULD_NOT_OPEN 2
+#define COULD_NOT_WRITE 3
+#define BROKEN_FORMAT 4
+#define INVALID_PARAM 5
 
 /* time measurement variables */
 struct timeval start_time;       /* time when program started               */
 struct timeval comp_time;        /* time when calculation complet           */
+
+struct MatrixInfo {
+	int threads;
+	int iteration;
+};
+
+/* ************************************************************************ */
+/*  Define log functions                                                    */
+/* ************************************************************************ */
+int log_color(char* code, char* msg) {
+	// TODO: Check for negative values here
+	fprintf(stderr, "%s", code);
+	fprintf(stderr, "%s", msg);
+	fprintf(stderr, "\x1b[0m\n");
+	return 0;
+}
+
+int log_warn(char* msg) {
+	return log_color("\x1b[1;33m", msg);
+}
+
+int log_err(char* msg) {
+	return log_color("\x1b[1;31m", msg);
+}
+
+int log_info(char* msg) {
+	return log_color("\x1b[32m", msg);
+}
 
 /* ************************************************************************ */
 /*  allocate matrix of size N x N                                           */
@@ -50,6 +87,16 @@ alloc_matrix (void)
 	}
 
 	return matrix;
+}
+
+/* ************************************************************************ */
+/*  offsets                                                                 */
+/* ************************************************************************ */
+
+const int isch = sizeof(int) * 3;
+
+unsigned long calc_offset(int row) {
+	return row * N * sizeof(double) + isch;
 }
 
 /* ************************************************************************* */
@@ -91,12 +138,62 @@ init_matrix (double** matrix)
 /* ************************************************************************* */
 /*  init matrix reading file					         	                             */
 /* ************************************************************************* */
+
+// I will not hold this as a global value, I want well-defined functions
 static
 void
-read_matrix (double** matrix)
+read_matrix (double** matrix, struct MatrixInfo* info, char* path)
 {
-	(void)matrix;
-	// TODO read
+	int file;
+	if ((file = open(path, O_RDONLY)) == -1) {
+		log_warn("Could not open checkpoint file, assuming it is uninitalized..");
+	}
+
+	int threads = 0;
+	int iteration_total = 0;
+	int iteration_completed = 0;
+
+	if ((pread(file, &threads, sizeof(int), 0)) == -1) {
+		log_err("Reading of threads in file failed");
+		exit(BROKEN_FORMAT);
+	}
+
+	if (threads != info->threads) {
+		log_err("Thread amount differes from checkpoint file");
+		exit(INVALID_PARAM);
+	}
+
+	if (pread(file, &iteration_total, sizeof(int), sizeof(int)) == -1) {
+		log_err("Reading of iteration total in file failed");
+		exit(BROKEN_FORMAT);
+	}
+
+	if (pread(file, &iteration_completed, sizeof(int), sizeof(int) * 2) == -1) {
+		log_err("Reading of iterations completed in file failed");
+		exit(BROKEN_FORMAT);
+	}
+
+	// If the amount of completed iterations are lower than the total iterations in the header, continue...
+
+	if (iteration_completed >= iteration_total && info->iteration <= iteration_total) {
+		log_err("All iterations are completed and new iteration count given is smaller or equal than exisiting count");
+		exit(INVALID_PARAM);
+	}
+
+	if (iteration_completed >= iteration_total && info->iteration > iteration_total) {
+		log_info("Checkpoints have been completed before, but the current invocation increases the total iterations");
+	}
+
+	for (size_t row = 0; row < N; row += 1) {
+		// printf("Reading row %ld\n", row);
+		// printf("p: %p, read: %ld, offset: %ld\n", (void*)matrix[row], N * sizeof(double), calc_offset(row));
+		if (pread(file, matrix[row], N * sizeof(double), calc_offset(row)) == -1) {
+			printf("Reason (errno): %d\n", errno);
+			log_err("Could not read matrix values");
+			exit(BROKEN_FORMAT);
+		}
+	}
+	close(file);
 }
 
 /* ************************************************************************ */
@@ -104,7 +201,7 @@ read_matrix (double** matrix)
 /* ************************************************************************ */
 static
 void
-calculate (double** matrix, int iterations, int threads)
+calculate (double** matrix, int iterations, int threads, char* path)
 {
 	int i, j, k, l;
 	int tid;
@@ -112,6 +209,24 @@ calculate (double** matrix, int iterations, int threads)
 
 	tid = 0;
 	lines = from = to = -1;
+	int file_descriptor;
+	// open is WELL defined
+	if ((file_descriptor = open(path, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR, 420/69)) == -1) {
+		log_err("Could not open checkpoint file for writing");
+		exit(COULD_NOT_OPEN);
+	}
+
+	// Write amount of threads
+	if (pwrite(file_descriptor, &threads, sizeof(int), 0) == -1) {
+		log_err("Could not write to checkpoints");
+		exit(COULD_NOT_WRITE);
+	}
+
+	// Write total iterations
+	if (pwrite(file_descriptor, &iterations, sizeof(int), sizeof(int) * 1) == -1) {
+		log_err("Could not write to checkpoints");
+		exit(COULD_NOT_WRITE);
+	}
 
 	// Explicitly disable dynamic teams
 	omp_set_dynamic(0);
@@ -138,11 +253,26 @@ calculate (double** matrix, int iterations, int threads)
 				}
 			}
 
-			// TODO write
-
+			// We will use pwrite in this case due to the advantages in parallel environments as described in `checkpoint-answers.txt`
+			// Also since formatting is no problem, we will simply use byte offsets to differentiate between numbers and provide no human readable
+			for (i = from; i < to; i++) {
+				// printf("matrix[%d]: %p, size: %ld, offset: %ld, tid: %d\n", i, (void*)matrix[i], sizeof(double)*N, (i * N * sizeof(double)) + isch, tid);
+				if (pwrite(file_descriptor, matrix[i], sizeof(double) * N, calc_offset(i)) == -1) {
+					log_err("Could not write to checkpoints");
+					exit(COULD_NOT_WRITE);
+				}
+			}
 			#pragma omp barrier
+			// write by first thread, this just preserves use from heaving T write access for a single opeation which block each other
+			if (tid == 0) {
+				if (pwrite(file_descriptor, &k, sizeof(int), sizeof(int) * 2) == -1) {
+					log_err("Could not write to checkpoints");
+					exit(COULD_NOT_WRITE);
+				}
+			}
 		}
 	}
+	close(file_descriptor);
 }
 
 /* ************************************************************************ */
@@ -166,24 +296,31 @@ int
 main (int argc, char** argv)
 {
 	int threads, iterations;
+	char* path = calloc(256, sizeof(char));
 	double** matrix;
 
-	if (argc < 3)
+
+	if (argc < 4)
 	{
-		printf("Usage: %s threads iterations\n", argv[0]);
+		printf("Usage: %s threads iterations path\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		sscanf(argv[1], "%d", &threads);
 		sscanf(argv[2], "%d", &iterations);
+		sscanf(argv[3], "%s", path);
 	}
+
+	struct MatrixInfo info = {threads, iterations};
 
 	matrix = alloc_matrix();
 
-	if (0)
+	int file;
+	if ((file = open(path, 0)) != -1)
 	{
-		read_matrix(matrix);
+		close(file);
+		read_matrix(matrix, &info, path);
 	}
 	else
 	{
@@ -191,12 +328,13 @@ main (int argc, char** argv)
 	}
 
 	gettimeofday(&start_time, NULL);
-	calculate(matrix, iterations, threads);
+	calculate(matrix, iterations, threads, path);
 	gettimeofday(&comp_time, NULL);
 
 	displayStatistics();
 
 	free_matrix(matrix);
+	free(path);
 
 	return 0;
 }
